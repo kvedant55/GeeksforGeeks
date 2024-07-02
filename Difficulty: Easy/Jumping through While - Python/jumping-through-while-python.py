56f83acdc9054286f43d12bd832d9428165e7d3a
Vedant Kalambe <114499548+kvedant55@users.noreply.github.com>
2024-07-03 00:58:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Jumping through While - Python/jumping-through-while-python.py
@@ -0,0 +1,43 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+# Function to print x in increasing order
+def printIncreasingPower(x):
+    ##Your code here
+    i = 1
+    # Loop to jump in powers of 2
+    while(pow(i,2) <= x):
+        
+        ##Your code here
+        
+        print ( pow(i,2), end = " ")
+        i = i + 1
+        ##Your code here
+
+
+
+#{ 
+ # Driver Code Starts.
+# Driver Code
+def main():
+    
+    # Testcase input
+    testcases = int(input())
+    
+    # Looping through testcases
+    while(testcases > 0):
+        x = int(input())
+        
+        printIncreasingPower(x);
+        print ()
+        
+        
+        testcases -= 1
+ 
+if __name__ == '__main__':
+    main()
+# } Driver Code Ends
